87e28a7b1dae2ad00dfe12cd8d970d162660c2b0
btoktogaziev <btoktogaziev@nurtelecom.kg>
2025-10-13 17:43:27 +0600

    First look of layout

--- app/src/main/java/com/example/calculatorcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorcomposeapp/MainActivity.kt
@@ -11,15 +11,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.calculatorcomposeapp.ui.screens.CalcButton
+import com.example.calculatorcomposeapp.ui.screens.CalculatorScreen
 import com.example.calculatorcomposeapp.ui.theme.CalculatorComposeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CalculatorComposeAppTheme {
-
+                CalculatorScreen()
             }
         }
     }
---
+++ app/src/main/java/com/example/calculatorcomposeapp/ui/screens/CalculatorScreen.kt
@@ -0,0 +1,197 @@
+package com.example.calculatorcomposeapp.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import com.example.calculatorcomposeapp.ui.theme.LightBlue
+import com.example.calculatorcomposeapp.ui.theme.LightGrayBg
+import com.example.calculatorcomposeapp.ui.theme.PrimePink
+import kotlinx.coroutines.launch
+
+@Composable
+fun CalculatorScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Row(
+            modifier = Modifier
+                .weight(0.8f)
+                .background(
+                    color = LightGrayBg,
+                    shape = RoundedCornerShape(
+                        topStart = 0.dp,
+                        topEnd = 0.dp,
+                        bottomStart = 32.dp,
+                        bottomEnd = 32.dp
+                    )
+                )
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            TextContainer()
+        }
+        Box(modifier = Modifier.weight(1.5f)) {
+            ButtonContainer()
+        }
+    }
+}
+
+@Composable
+fun TextContainer() {
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect("12345678912345") {
+        coroutineScope.launch {
+            scrollState.scrollTo(scrollState.maxValue)
+        }
+    }
+
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 8.dp)
+            .horizontalScroll(state = scrollState),
+        text = "0",
+        textAlign = TextAlign.End,
+        maxLines = 1,
+        style = TextStyle(
+            fontSize = 48.sp,
+        )
+    )
+}
+
+@Composable
+fun CalcButton(
+    text: String,
+    color: Color,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = Modifier
+            .size(92.dp),
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(color),
+    ) {
+        Text(
+            text = text,
+            style = TextStyle(
+                letterSpacing = (-0.025).em,
+                fontSize = 32.sp,
+                color = Color.Black,
+                fontFamily = FontFamily.Monospace
+            )
+        )
+    }
+}
+
+@Composable
+fun CalcExtraButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    Button(
+        modifier = Modifier.size(64.dp),
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(Color.Transparent),
+    ) {
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                fontSize = 24.sp,
+                color = Color.Black,
+                fontFamily = FontFamily.Monospace
+            )
+        )
+    }
+}
+
+@Composable
+fun ButtonContainer() {
+    val buttons = listOf<List<String>>(
+        listOf("AC", "()", "%", "÷"),
+        listOf("7", "8", "9", "×"),
+        listOf("4", "5", "6", "—"),
+        listOf("1", "2", "3", "+"),
+        listOf("0", "•", "⌫", "=")
+    )
+    val extraButtons = listOf("√", "π", "^", "!", "▵")
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(4.dp),
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.6f)
+                .padding(vertical = 4.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            extraButtons.forEach {
+                CalcExtraButton(text = it) { }
+            }
+        }
+
+
+        buttons.forEach { row ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                row.forEach {
+                    CalcButton(
+                        text = it,
+                        color = if (it == "AC") {
+                            PrimePink
+                        } else if (it == "()" || it == "%" || it == "÷" || it == "+" || it == "=" || it == "×" || it == "—") {
+                            LightBlue
+                        } else LightGrayBg
+                    ) {
+
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun ButtonPreview() {
+    CalculatorScreen()
+}
