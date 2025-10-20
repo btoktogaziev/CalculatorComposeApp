f3523666d58de822d1345bc27be5e68e157dd0f8
btoktogaziev <btoktogaziev@nurtelecom.kg>
2025-10-20 14:04:15 +0600

    Оптимизация импортов

--- app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/appbar/AppBar.kt
+++ app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/appbar/AppBar.kt
@@ -1,12 +1,10 @@
 package com.example.calculatorcomposeapp.presentation.ui.appbar
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -27,9 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculatorcomposeapp.R
 import com.example.calculatorcomposeapp.presentation.ui.theme.RadioButtonColor
