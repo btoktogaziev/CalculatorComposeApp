f50f754ef45978827c5b58cd136797fcd18179f0
btoktogaziev <btoktogaziev@nurtelecom.kg>
2025-10-20 09:46:46 +0600

    Смена цветов

--- app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/theme/Theme.kt
@@ -8,28 +8,24 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPink,
+    secondary = DarkBlue,
+    tertiary = DarkGrayBg,
+    onSurface = Color.White,
+    background = Color.Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimePink,
+    secondary = LightBlue,
+    tertiary = LightGrayBg,
+    onSurface = Color.Black,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    background = Color.White
 )
 
 @Composable
