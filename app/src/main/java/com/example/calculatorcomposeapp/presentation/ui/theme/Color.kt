5d1dc2740c76c5cdece3d7f2883bdbcb334946b2
btoktogaziev <btoktogaziev@nurtelecom.kg>
2025-10-20 09:47:12 +0600

    Подбор цветов

--- app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/theme/Color.kt
@@ -3,14 +3,10 @@ package com.example.calculatorcomposeapp.presentation.ui.theme
 import android.hardware.lights.Light
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val LightGrayBg = Color(0xffdde3ef)
 val LightBlue = Color(0xffd5e4f7)
 val PrimePink = Color(0xffefdbff)
+
+val DarkGrayBg = Color(0xff1e1e2e)
+val DarkBlue = Color(0xff1e293b)
+val DarkPink = Color(0xff581c87)
