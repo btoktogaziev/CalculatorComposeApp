49ce3d973d9856d333f9f659f9ccedfafa12f35f
btoktogaziev <btoktogaziev@nurtelecom.kg>
2025-10-20 10:49:20 +0600

    Добавил выбор темы

--- app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/activity/MainActivity.kt
@@ -3,8 +3,13 @@ package com.example.calculatorcomposeapp.presentation.ui.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.calculatorcomposeapp.presentation.ui.appbar.AppBar
@@ -15,9 +20,19 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CalculatorComposeAppTheme {
+            var selectedTheme by remember { mutableStateOf("System") }
+            val darkTheme = when (selectedTheme) {
+                "Light" -> false
+                "Dark" -> true
+                else -> isSystemInDarkTheme()
+            }
+            CalculatorComposeAppTheme(darkTheme = darkTheme) {
                 Scaffold(
-                    topBar = { AppBar() }
+                    topBar = {
+                        AppBar(onThemeSelected = { theme ->
+                            selectedTheme = theme
+                        })
+                    }
                 ) {
                     CalculatorScreen(
                         Modifier.Companion.padding(it),
--- app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/appbar/AppBar.kt
+++ app/src/main/java/com/example/calculatorcomposeapp/presentation/ui/appbar/AppBar.kt
@@ -1,34 +1,142 @@
 package com.example.calculatorcomposeapp.presentation.ui.appbar
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculatorcomposeapp.R
 
 @Composable
-fun AppBar() {
+fun AppBar(onThemeSelected: (String) -> Unit) {
+    var opened by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
     @OptIn(ExperimentalMaterial3Api::class)
     TopAppBar(
         title = { Text("") },
         actions = {
             IconButton(
                 modifier = Modifier.padding(top = 40.dp),
-                onClick = {}) {
+                onClick = {
+                    opened = true
+                }) {
                 Icon(
                     painter = painterResource(R.drawable.ic_menu),
                     contentDescription = "Menu"
                 )
             }
+            DropdownMenu(
+                expanded = opened,
+                onDismissRequest = { opened = false }
+            ) {
+                DropdownMenuItem(
+                    text = {
+                        Text(
+                            modifier = Modifier.padding(end = 12.dp),
+                            text = "Выбрать тему"
+                        )
+                    },
+                    onClick = {
+                        showDialog = true
+                        opened = false
+                    }
+                )
+            }
         },
         colors = TopAppBarDefaults.topAppBarColors(MaterialTheme.colorScheme.tertiary)
     )
+    if (showDialog) {
+        AlertDialogForTheme(
+            onThemeSelected = onThemeSelected,
+            onDismiss = { showDialog = false }
+        )
+    }
+}
+
+@Composable
+fun AlertDialogForTheme(onThemeSelected: (String) -> Unit, onDismiss: () -> Unit) {
+    var selectedItem by remember { mutableStateOf("Light") }
+
+    AlertDialog(
+        icon = {
+            Icon(
+                imageVector = Icons.Default.Settings,
+                contentDescription = "Theme"
+            )
+        },
+        title = {
+            Text(text = "Выбор темы")
+        },
+        text = {
+            Column {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = selectedItem == "Light",
+                        onClick = { selectedItem = "Light" }
+                    )
+                    Text("Светлая тема", modifier = Modifier.padding(start = 8.dp))
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = selectedItem == "Dark",
+                        onClick = { selectedItem = "Dark" }
+                    )
+                    Text("Тёмная тема", modifier = Modifier.padding(start = 8.dp))
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = selectedItem == "System",
+                        onClick = { selectedItem = "System" }
+                    )
+                    Text("Системная тема", modifier = Modifier.padding(start = 8.dp))
+                }
+            }
+        },
+        onDismissRequest = {
+            onDismiss
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onThemeSelected(selectedItem)
+                    onDismiss
+                }
+            ) {
+                Text("Ок")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismiss
+                }
+            ) {
+                Text("Отмена")
+            }
+        })
 }
